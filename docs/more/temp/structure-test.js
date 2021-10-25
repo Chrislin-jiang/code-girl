ebd6ccd54349eb562f5ba0fd2335d5fae354aad4
gongjianglin <gongjlg@yonyou.com>
2021-10-25 22:57:28 +0800

    914-卡牌分组-正确

--- docs/more/temp/structure-test.js
+++ docs/more/temp/structure-test.js
@@ -1,47 +1,47 @@
 /**
  * @param {number[]} deck
  * @return {boolean}
- * [1,2,3,4,4,3,2,1]
- * [1,1,1,2,2,2,3,3]
- * [1,1,1,2,2,2,3,3]
  */
-var hasGroupsSizeX = function(deck) {
+ const hasGroupsSizeX = function(deck) {
   if (deck.length === 1) {
     return false;
   } else {
+    // 最大公约数
+    const gcdFn = (a, b) => {
+      // 利用辗转相除法来计算最大公约数
+      let c = a % b;
+      if (c === 0) return b;
+      return gcdFn(b, c);
+    };
     deck.sort((a, b) => a - b);
-    let count = 0;
-    let temp = 1;
+    let count = 1;
+    let gcd;
     for (let i = 1; i < deck.length; i++) {
       if (deck[i] === deck[i - 1]) {
-        temp = temp + 1;
+        count++;
       } else {
-        if (count === 0) {
-          count = temp;
-        }
-        if (temp >= count && temp % count === 0) {
-          temp = 1;
-        } else if (temp < count && count % temp === 0) {
-          count = temp;
-          temp = 1;
+        if (count === 1) {
+          return false;
         } else {
-          continue;
+          if (gcd === undefined) {
+            gcd = count;
+          } else {
+            gcd = gcdFn(gcd, count);
+          }
+          count = 1;
         }
       }
     }
-    if (count === 0) {
+    // 最后
+    if (gcd === undefined) {
+      gcd = count;
+    } else {
+      gcd = gcdFn(gcd, count);
+    }
+    if (gcd >= 2) {
       return true;
     } else {
-      if (temp >= count && temp % count === 0) {
-        return true;
-      } else if (temp < count && count % temp === 0) {
-        return true;
-      } else {
-        return false;
-      }
+      return false;
     }
   }
 };
-
-// 这个方法不行
-// [1,1,1,1,2,2,2,2,2,2]   temp=6，count=4，但是 n 可以取 2
